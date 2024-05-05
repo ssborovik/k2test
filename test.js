9a4f31a5379d35c95395fb5117d5a5848a7f018d
ssborovik <92776983+ssborovik@users.noreply.github.com>
2024-05-05 08:40:49 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Находим элемент кнопки календаря по id
     var calendarButton = document.getElementById("88f8ded8-52e8-2e04-5e14-1f15a41083c8_bc331c7d-62c0-706e-e91c-a4cbe9ab1cc3_Image");
 
     // Добавляем обработчик события click
