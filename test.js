223c7ae06eb997066786d37d253d2c3d6bea593f
ssborovik <92776983+ssborovik@users.noreply.github.com>
2024-05-05 08:41:39 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var calendarButton = document.getElementById("88f8ded8-52e8-2e04-5e14-1f15a41083c8_bc331c7d-62c0-706e-e91c-a4cbe9ab1cc3_Image");
-
-    // Добавляем обработчик события click
-    calendarButton.addEventListener("click", function() {
-        alert("Привет");
-    });
-});
+console.log('sergey');
