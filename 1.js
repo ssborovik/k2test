4c0024005e98a8a49a95c6ffa1b2e7bd5e178c35
ssborovik <92776983+ssborovik@users.noreply.github.com>
2024-05-07 11:02:37 +0300

    Delete 1.js

--- 1.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("54176b00-26fa-8beb-47aa-f8042a0c1761_c7c7f8b1-fb18-c79e-4892-5e79e1fb2852_ToolbarButton").addEventListener("click", function() {
-    alert("Кнопка была нажата!");
-});
